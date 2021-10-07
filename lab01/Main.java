6b31a50beed972825357143ef02efd85a139033a
Vast7al (Szymon Świerzbin) <73667837+Vast7al@users.noreply.github.com>
2021-10-07 21:36:01 +0200

    Lab01 - updated.

---
+++ lab01/Main.java
@@ -0,0 +1,69 @@
+package pl.edu.uwm.wmii.kotewa.laboratorium00;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //zadanie 1
+        System.out.println(31+28+27);
+        //zadanie 2
+        int sum = 0;
+        for (int i=0;i<=10;i++)
+        {
+            sum+=i;
+        }
+        System.out.println(sum);
+        //zadanie 3
+        int iloczyn = 1;
+
+        for(int i = 1; i <= 10; ++i) {
+            iloczyn *= i;
+        }
+
+        System.out.println(iloczyn);
+        //zadanie 4
+        float saldoKonta = 1000;
+
+        for(int i = 1; i <= 3; ++i) {
+            saldoKonta = saldoKonta+=saldoKonta*0.06;
+            System.out.println(i + " rok = " + saldoKonta);
+        }
+        //zadanie 5
+        System.out.println("______");
+        System.out.println("|Java|");
+        System.out.println("+----+");
+        //zadanie 6
+        System.out.println("   /////    \n  +\"\"\"\"\"+  \n (| o o |)\n   | ^ |   \n  | `-' |  \n  +-----+  \n");
+        //zadanie 7 //uzyje swojego drugiego imienia :)
+        System.out.println("                        **********                                                                     ");
+        System.out.println("*     *   ****       *      *     ******    *  *     ****                                         ");
+        System.out.println("*     *  *    *      *      *     *         * *     *                                  ");
+        System.out.println("*     *  *    *      *      *     ******    **       ****                                                      ");
+        System.out.println("*  *  *   *    *     *      *     *         **          *                                ");
+        System.out.println("* * * *   *    *     *      *     *         * *      ***  .                                                ");
+        System.out.println(" *   *     ****   ****      *     ******    *  *                                                                ");
+        //zadanie 8
+        System.out.println("   +    \n  + +   \n +   +  \n+-----+\n| .-. |\n| | | |\n+-+-+-+\n");
+        //zadanie 9
+        System.out.println(" (-|-)        -----\n |   |       /Hello\\\n |   |       <Junior|\n  | |        \\Coder!/\n              -----");
+        //zadanie 10
+        System.out.println("1.Cowboy Bebop\n2.Mushishi\n3.Monster");
+        //zadanie 11
+        System.out.println("Klasyk\nKilka grud ziemi, a będzie zapomniane życie.\nMuzyka wyswobodzi się z okoliczności.");
+        //zadanie 12
+        System.out.println("* * * * * * ===========================================");
+        System.out.println(" * * * * *  ===========================================");
+        System.out.println("* * * * * * ===========================================");
+        System.out.println(" * * * * *  ===========================================");
+        System.out.println("* * * * * * ===========================================");
+        System.out.println(" * * * * *  ===========================================");
+        System.out.println("* * * * * * ===========================================");
+        System.out.println(" * * * * *  ===========================================");
+        System.out.println("* * * * * * ===========================================");
+        System.out.println("=======================================================");
+        System.out.println("=======================================================");
+        System.out.println("=======================================================");
+        System.out.println("=======================================================");
+        System.out.println("=======================================================");
+        System.out.println("=======================================================");
+    }
+}
