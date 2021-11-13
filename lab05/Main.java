6124b9e0dffcb27d73dbf49e3c446e78fae77f2a
Vast7al (Szymon Świerzbin) <73667837+Vast7al@users.noreply.github.com>
2021-11-13 14:04:01 +0100

    Added lab05.

---
+++ lab05/Main.java
@@ -0,0 +1,151 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Integer> tablica = new ArrayList<Integer>();
+        tablica.add(1);
+        tablica.add(2);
+        tablica.add(3);
+        ArrayList<Integer> tablica2 = new ArrayList<Integer>();
+        tablica2.add(4);
+        tablica2.add(5);
+        tablica2.add(6);
+        tablica2.add(7);
+        tablica2.add(8);
+        //Zadanie 1
+//        ArrayList<Integer> tablica3 = append(tablica,tablica2);
+//
+        //Zadanie 2
+//        ArrayList<Integer> tablica3 = merge(tablica, tablica2);
+
+//        for(int i = 0; i <=tablica3.size()-1; i++){
+//           System.out.println(tablica3.get(i));
+//        }
+        //Zadanie 3
+//        ArrayList<Integer> tablica3 = mergeSorted(tablica, tablica2);
+//        for(int i = 0; i <=tablica3.size()-1; i++) {
+//            System.out.println(tablica3.get(i));
+//        }
+
+        //Zadanie 4
+//        ArrayList<Integer> tablica3 = reversed(mergeSorted(tablica,tablica2));
+//        for(int i = 0; i <=tablica3.size()-1; i++) {
+//            System.out.println(tablica3.get(i));
+//        }
+
+        //Zadanie 5
+
+//        ArrayList<Integer> tablica3 = mergeSorted(tablica, tablica2);
+//        for(int i = 0; i <=tablica3.size()-1; i++) {
+//            System.out.println(tablica3.get(i));
+//        }
+//        reverse(tablica3);
+//        for(int i = 0; i <=tablica3.size()-1; i++) {
+//            System.out.println(tablica3.get(i));
+//        }
+
+
+
+
+    }
+
+    //funkcja do zadania 1
+    public static ArrayList<Integer> append(ArrayList<Integer> a, ArrayList<Integer> b){
+        ArrayList<Integer> tablica = a;
+
+        tablica.addAll(b);
+
+
+        return tablica;
+    }
+
+
+
+
+}
+    //funkcja do zadania 2
+    public static ArrayList<Integer> merge(ArrayList<Integer> a, ArrayList<Integer> b){
+        ArrayList<Integer> tablica = new ArrayList<Integer>();
+        ArrayList<Integer> tablica2 = new ArrayList<Integer>();
+        int size1 = a.size();
+        int size2 = b.size();
+        int iterator = 0;
+        if(size1 >= size2) {
+            iterator = size2;
+
+            tablica2 = a;
+        }
+        if(size2 >= size1) {
+            iterator = size1;
+
+            tablica2 = b;
+        }
+
+        for(int i = 0; i<=iterator-1; i++ ){
+            tablica.add(a.get(i));
+            tablica.add(b.get(i));
+
+        }
+
+        for(int i = iterator; i <=tablica2.size()-1; i++){
+            tablica.add(tablica2.get(i));
+        }
+
+
+
+
+        return tablica;
+    }
+
+    //funkcja do zadania 3
+    public static ArrayList<Integer> mergeSorted(ArrayList<Integer> a, ArrayList<Integer> b){
+        ArrayList<Integer> tablica = new ArrayList<Integer>();
+        tablica = merge(a, b);
+        Collections.sort(tablica);
+
+        return tablica;
+    }
+
+    //funkcja do zadania 4
+    public static ArrayList<Integer> reversed(ArrayList<Integer> a){
+        ArrayList<Integer> tablica = new ArrayList<Integer>();
+
+        int[] arr = new int[a.size()];
+
+        for(int i = 0; i <=a.size()-1; i++){
+            arr[arr.length-i-1] = a.get(i);
+
+        }
+        for(int i = 0; i <=a.size()-1; i++){
+            tablica.add(arr[i]);
+
+        }
+
+
+
+
+        return tablica;
+    }
+    //funkcja do zadania 5
+    public static void reverse(ArrayList<Integer> a){
+
+        int[] arr = new int[a.size()];
+        for(int i = 0; i <=a.size()-1; i++){
+            arr[arr.length-i-1] = a.get(i);
+
+        }
+
+        for(int i = 0; i <=a.size()-1; i++){
+            a.set(i, arr[i]);
+
+
+
+        }
+
+
+
+    }
+
+}
