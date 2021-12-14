0ba2b9dd75575d9391664071a248402f9befde98
Vast7al (Szymon Świerzbin) <73667837+Vast7al@users.noreply.github.com>
2021-12-14 19:25:50 +0100

    Added lab08.

---
+++ lab08/Instrumenty/Flet.java
@@ -0,0 +1,15 @@
+package pl.szymonswierzbin.Instrumenty;
+import java.time.LocalDate;
+public class Flet extends Instrument{
+
+
+    public Flet(String producent, LocalDate rokProdukcji) {
+        super(producent, rokProdukcji);
+    }
+
+    @Override
+    public void dzwiek() {
+        System.out.println("fululu");
+
+    }
+}
---
+++ lab08/Instrumenty/Fortepian.java
@@ -0,0 +1,12 @@
+package pl.szymonswierzbin.Instrumenty;
+import java.time.LocalDate;
+public class Fortepian extends Instrument {
+    public Fortepian(String producent, LocalDate rokProdukcji) {
+        super(producent, rokProdukcji);
+    }
+
+    @Override
+    public void dzwiek() {
+        System.out.println(Tuduuuum);
+    }
+}
