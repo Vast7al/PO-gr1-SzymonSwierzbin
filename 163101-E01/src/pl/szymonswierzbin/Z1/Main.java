f179886afbd106261c9e6170b4a50c150cb14ed8
Vast7al (Szymon Świerzbin) <73667837+Vast7al@users.noreply.github.com>
2022-01-31 18:05:02 +0100

    Added commit.

--- 163101-E01/src/pl/szymonswierzbin/Z1/Main.java
+++ 163101-E01/src/pl/szymonswierzbin/Z1/Main.java
@@ -1,10 +1,48 @@
 package pl.szymonswierzbin.Z1;
+
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Stream;
-public class Main {
-    public static void main(String[] args)throws Exception{
-        Arraylist<Integer>lista=new ArrayList<Integer>(
 
+public class Main {
+public void main(String[] args) throws Exception{
+    ArrayList<Integer> list=new ArrayList<Integer>(
+     Arrays.asList(1,2,3,4,5,6,7));
+    ArrayList<Integer> list2=new ArrayList<Integer>(
+     Arrays.asList(1,2,3,2,1));
+    if (Palindrom.jestPalindronem(list)) {
+        System.out.println("Jest palindronem");
+        }
+    else{
+        System.out.println("Nie jest");
+    }
+    if (Palindrom.jestPalindronem(list2)) {
+        System.out.println("Jest palindronem");
+    }
+    else{
+        System.out.println("Nie jest");
+    }
+    ArrayList<LocalTime> lista3=new ArrayList<LocalTime>(
+            Arrays.asList(
+                    LocalTime.of(10,43,12),
+                    LocalTime.of(10,11,12),
+                    LocalTime.of(10,43,12)));
+    ArrayList<LocalTime> lista4=new ArrayList<LocalTime>(
+            Arrays.asList(
+            LocalTime.of(10,43,12),
+            LocalTime.of(10,11,12),
+            LocalTime.of(10,43,11)));
+    if (Palindrom.jestPalindronem(lista3)) {
+        System.out.println("Jest palindronem");
+    }
+    else{
+        System.out.println("Nie jest");
+    }
+    if (Palindrom.jestPalindronem(lista4)) {
+        System.out.println("Jest palindronem");
+    }
+    else{
+        System.out.println("Nie jest");
+    }
+}
 }
--- 163101-E01/src/pl/szymonswierzbin/Z1/Palindrom.java
+++ 163101-E01/src/pl/szymonswierzbin/Z1/Palindrom.java
@@ -1,16 +1,18 @@
 package pl.szymonswierzbin.Z1;
-
+import java.time.LocalTime;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Palindrom {
-    public static <T extends Comparable> boolean jestPalindronem(ArrayList<T>tab){
-     int i=0,j=tab.size()-1;
-       while (i<j){
-       if(tab.get(i)!=tab.get(j))
-           return false;
-           i++;
-           j--;
-       }
-       return true;
+    public static <T extends Comparable> boolean jestPalindronem(ArrayList<T> tab) {
+        int i = 0, j = tab.size() - 1;
+        while (i < j) {
+            if (tab.get(i) != tab.get(j))
+                return false;
+            i++;
+            j--;
+        }
+        return true;
     }
 }
+
